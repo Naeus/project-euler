fae1047560ee16336d7158da199ec09df5e4e7a0
DESKTOP-FUAAG0C\username <naelonemaxwell@gmail.com>
2018-04-28 18:19:15 +0300

    Problem 1 - Multiples-of-two-relatively-prime-numbers

---
+++ Problem-1_Multiples-of-3-and-5/Problem-1_Multiples-of-3-and-5/Program.fs
@@ -0,0 +1,15 @@
+﻿let sumFormula n:int =
+    n * (n + 1) / 2
+
+let sumMultiples m n =
+    let quantityOfNumbers = int(ceil(float(n) / float(m))) - 1
+    int(m * (sumFormula quantityOfNumbers))
+
+let sumTwoRelativelyPrimeMultiples m1 m2 n =
+    sumMultiples m1 n + sumMultiples m2 n - sumMultiples (m1 * m2) n
+
+// Use the EntryPoint attribute to run the program.
+[<EntryPoint>]
+let main args =
+    Printf.printfn "Sum of multiples of 3 and 5 less than 1000 is: %i" (sumTwoRelativelyPrimeMultiples 3 5 1000)
+    0
